f727a28aa42a96a6b68b4cbca0424bf0dd994d64
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2026-06-06 18:36:54 +0300

    fixs

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "313063053" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1323903977" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
