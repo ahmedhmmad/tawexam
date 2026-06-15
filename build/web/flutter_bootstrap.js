2a6b0d2c9dc17b86cfdc2449277d87e8a8cfeade
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2026-06-16 00:52:07 +0300

    Build admin web: activity log monitoring view

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3092050467" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "400557945" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
